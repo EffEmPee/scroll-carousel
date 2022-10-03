86b2e386940eb93b6cf01f908c980e069eb57e35
felipe pierotti <felipe@DESKTOP-NBU16Q3.localdomain>
2022-10-03 11:47:26 -0300

    feat: add scroll and keyboard events

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,121 @@
+/* eslint-disable consistent-return */
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import { useEffect, useRef, useState, WheelEventHandler } from 'react';
 
-import { Container } from '../styles/pages/Home';
+import { motion, useIsomorphicLayoutEffect, useTransform } from 'framer-motion';
 
-const Home: NextPage = () => (
-  <Container>
-    <Head>
-      <title>NextJS kit</title>
-    </Head>
+import { CarouselContainer, Container } from '../styles/pages/Home';
 
-    <h1>Hello world!</h1>
-  </Container>
-);
+// sections and necessary information
+const sections = [
+  {
+    title: 'Section 1',
+    color: '#313715'
+  },
+  {
+    title: 'Section 2',
+    color: '#D16014'
+  },
+  {
+    title: 'Section 3',
+    color: '#939F5C'
+  },
+  {
+    title: 'Section 4',
+    color: '#84828F'
+  }
+];
+
+const Home: NextPage = () => {
+  const ref = useRef<HTMLUListElement>(null);
+  const [containerHeight, setContainerHeight] = useState(0);
+  const [activeSection, setActiveSection] = useState(0);
+  const [isScrolling, setIsScrolling] = useState(false);
+
+  const buffer = 300;
+
+  const [tops, setTops] = useState([0]);
+
+  // get the container full height
+  useIsomorphicLayoutEffect(() => {
+    const container = ref.current;
+
+    if (!container) return;
+
+    const onResize = () => {
+      setContainerHeight(container.offsetHeight);
+    };
+
+    onResize();
+    window.addEventListener('resize', onResize);
+
+    return () => window.removeEventListener('resize', onResize);
+  }, []);
+
+  const startBuffer = () => {
+    setIsScrolling(true);
+    const timeout = setTimeout(() => setIsScrolling(false), buffer);
+  };
+
+  const nextSection = () => {
+    const next = activeSection + 1;
+    setActiveSection(next === sections.length ? activeSection : next);
+  };
+
+  const prevSection = () => {
+    const prev = activeSection - 1;
+    setActiveSection(prev === -1 ? activeSection : prev);
+  };
+
+  const onWheel = (event: WheelEvent) => {
+    if (isScrolling) return;
+    if (event.deltaY > 0) {
+      nextSection();
+    } else {
+      prevSection();
+    }
+    startBuffer();
+  };
+
+  const onKeyDown = (event: KeyboardEvent) => {
+    if (isScrolling) return;
+    if (event.key === 'ArrowDown') {
+      nextSection();
+    } else if (event.key === 'ArrowUp') {
+      prevSection();
+    }
+    startBuffer();
+  };
+
+  return (
+    <Container>
+      <Head>
+        <title>Scroll Carousel</title>
+      </Head>
+
+      <CarouselContainer
+        ref={ref}
+        tabIndex={0}
+        onWheel={onWheel as any}
+        onKeyDown={onKeyDown as any}
+      >
+        {sections.map((section, index) => (
+          <motion.li
+            style={{ backgroundColor: section.color }}
+            animate={{
+              top: activeSection >= index ? 0 : containerHeight * 2
+              // scale: activeSection >= index ? 1 : 1
+              // scaleX: activeSection >= index ? 1 : 0.1
+            }}
+            transition={{ type: 'easeInOut', duration: 0.8 }}
+          >
+            <h1>{section.title}</h1>
+          </motion.li>
+        ))}
+      </CarouselContainer>
+    </Container>
+  );
+};
 
 export default Home;
--- src/styles/pages/Home.ts
+++ src/styles/pages/Home.ts
@@ -1,12 +1,32 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  /* height: 100vh; */
 
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
   background-color: #f5f5f5;
+`;
+
+export const CarouselContainer = styled.ul`
+  width: 100%;
+  height: 100vh;
+  overflow: hidden;
+  position: relative;
+
+  li {
+    position: absolute;
+    width: 100%;
+    height: 100vh;
+
+    display: flex;
+
+    h1 {
+      margin: auto;
+      color: white;
+    }
+  }
 `;
